abf7744a1c1f9da29107352543044b7b01bf778f
meekmarcelin <meekmarcelin@gmail.com>
2023-02-20 12:48:19 -0800

    web

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- Create db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- add a user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
