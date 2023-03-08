9f36d7fc6a825c8a25636dd8cb4e232e830da1f7
meekmarcelin <meekmarcelin@gmail.com>
2023-03-08 08:30:47 -0800

    1 commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" do pack and generates an archive """
+
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Create a tgz archive of the web_static folder"""
+    try:
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        archive_path = "versions/web_static_{}.tgz".format(current_time)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
+        return None
+
+
