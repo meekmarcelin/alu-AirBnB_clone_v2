dbe58b13ff50f6dd523a41d6e278b6ae9fe54471
meekmarcelin <meekmarcelin@gmail.com>
2023-03-09 01:01:57 -0800

    2 commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Fabric script to distribute an archive """
+
+
 from fabric.api import *
 from os.path import exists
 env.hosts = ['3.89.247.193', '54.211.187.147']
@@ -17,8 +19,7 @@ def do_deploy(archive_path):
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(filename, folder_name))
         run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(folder_name, folder_name))
+        run("mv /data/web_static/releases/{}/web_static/* \data/web_static/releases/{}/".format(folder_name, folder_name))
         run("rm -rf /data/web_static/releases/{}/web_static".format(folder_name))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
@@ -27,4 +28,3 @@ def do_deploy(archive_path):
         return True
     except:
         return False
-
