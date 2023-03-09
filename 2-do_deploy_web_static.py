e0b676993bed927ca3730a9b5b08f3a9744d930e
meekmarcelin <meekmarcelin@gmail.com>
2023-03-09 01:14:10 -0800

    2 commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-""" Fabric script to distribute an archive """
+""" Fabric script to distribute archive """
 
 
-from fabric.api import *
+from fabric.api import run, env, put
 from os.path import exists
-env.hosts = ['3.89.247.193', '54.211.187.147']
+env.hosts = ['54.211.187.147', '23.23.62.187']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
+    """deploy web static"""
     if not exists(archive_path):
         return False
     try:
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split(".")[0]
+        path_name = "/data/web_static/releases/" + name
         put(archive_path, "/tmp/")
-        filename = archive_path.split("/")[-1]
-        folder_name = filename.split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(folder_name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(filename, folder_name))
-        run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* \data/web_static/releases/{}/".format(folder_name, folder_name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(folder_name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(folder_name))
-        print("New version deployed!")
+        run("mkdir -p {}/".format(folder_name))
+        run('tar -xzf /tmp/{} -C {}/'.format(file_name, folder_name))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}/web_static/* {}".format(folder_name, folder_name))
+        run("rm -rf {}/web_static".format(folder_name))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ /data/web_static/current'.format(folder_name))
         return True
     except:
         return False
