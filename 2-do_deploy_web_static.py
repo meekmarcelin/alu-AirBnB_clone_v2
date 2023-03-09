b5d1443bd3f7ce94fffb96864f18d653d442dce7
meekmarcelin <meekmarcelin@gmail.com>
2023-03-09 01:20:41 -0800

    2 commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ Fabric script to distribute archive """
-
-
 from fabric.api import run, env, put
 from os.path import exists
-env.hosts = ['54.211.187.147', '23.23.62.187']
+env.hosts = ['54.211.187.147', '3.89.247.193']
 
 
 def do_deploy(archive_path):
@@ -16,13 +14,13 @@ def do_deploy(archive_path):
         name = file_name.split(".")[0]
         path_name = "/data/web_static/releases/" + name
         put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(folder_name))
-        run('tar -xzf /tmp/{} -C {}/'.format(file_name, folder_name))
+        run("mkdir -p {}/".format(path_name))
+        run('tar -xzf /tmp/{} -C {}/'.format(file_name, path_name))
         run("rm /tmp/{}".format(file_name))
-        run("mv {}/web_static/* {}".format(folder_name, folder_name))
-        run("rm -rf {}/web_static".format(folder_name))
+        run("mv {}/web_static/* {}".format(path_name, path_name))
+        run("rm -rf {}/web_static".format(path_name))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}/ /data/web_static/current'.format(folder_name))
+        run('ln -s {}/ /data/web_static/current'.format(path_name))
         return True
     except:
         return False
